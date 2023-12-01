ecea1b77e3c1c0da3951e140f44acc97b34cb77c
Nickgem25 <88162454+Nickgem25@users.noreply.github.com>
2023-12-01 09:35:07 +0700

    Start
    Uploaded files

---
+++ Periodic table/Quiz/script.js
@@ -0,0 +1,102 @@
+const questions = [
+    {
+        question: "Q1",
+        answers: [
+            { text: "ans 1", correct: true},
+            { text: "ans 1", correct: false},
+            { text: "ans 1", correct: false},
+            { text: "ans 1", correct: false},
+        ]
+    },
+    {
+        question: "Q2",
+        answers: [
+            { text: "ans 1", correct: false},
+            { text: "ans 1", correct: true},
+            { text: "ans 1", correct: false},
+            { text: "ans 1", correct: false},
+        ]
+    }
+];
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo+ ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";        
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = 
+}
+
+function handleNextbutton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < question.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextbutton();
+    }else{
+        startQuiz();
+    }
+});
+
+
+startQuiz();
