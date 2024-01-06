ffa185edac2ed5d64376a8850de7cc4eae162fd6
Nickgem25 <nickgem25@gmail.com>
2024-01-06 23:42:29 +0700

    Got the hub :wink:
    got the hub
    made everything into (sorta) the same theme
    fixed some of the code for the table
    deleted flashcard

---
+++ script.js
@@ -0,0 +1,33 @@
+let windowObjectReference = null; // global variable
+let previousURL; /* global variable that will store the
+                    url currently in the secondary window */
+function openRequestedSingleTab(url) {
+  if (windowObjectReference === null || windowObjectReference.closed) {
+    windowObjectReference = window.open(url, "SingleSecondaryWindowName");
+  } else if (previousURL !== url) {
+    windowObjectReference = window.open(url, "SingleSecondaryWindowName");
+    /* if the resource to load is different,
+       then we load it in the already opened secondary window and then
+       we bring such window back on top/in front of its parent window. */
+    windowObjectReference.focus();
+  } else {
+    windowObjectReference.focus();
+  }
+  previousURL = url;
+  /* explanation: we store the current url in order to compare url
+     in the event of another call of this function. */
+}
+
+const links = document.querySelectorAll(
+  "a[target='SingleSecondaryWindowName']",
+);
+for (const link of links) {
+  link.addEventListener(
+    "click",
+    (event) => {
+      openRequestedSingleTab(link.href);
+      event.preventDefault();
+    },
+    false,
+  );
+}
